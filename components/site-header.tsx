52572d03cf3161b0ba644d4a4db2baf1df0bc7d8
faysal-cpu <faysal@ualberta.ca>
2026-02-27 12:58:30 -0500

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -44,7 +44,7 @@ export function SiteHeader() {
             width={440}
             height={59}
             priority
-            className="object-contain w-[300px] md:w-[360px] lg:w-[440px]"
+            className="object-contain w-[340px] md:w-[360px] lg:w-[440px]"
           />
         </Link>
 
